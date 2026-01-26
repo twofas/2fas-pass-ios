64c6c56fa2989979e6d320eae04586fd31e2b390
Maciej Szewczyk <maciej.szewczyk@icloud.com>
2026-01-26 19:38:28 +0100

    Add forgot password layout

--- 2PASS/CommonUI/ForgotMasterPassword/ForgotMasterPasswordView.swift
+++ 2PASS/CommonUI/ForgotMasterPassword/ForgotMasterPasswordView.swift
@@ -6,18 +6,41 @@
 
 import SwiftUI
 
+private struct Constants {
+    static let imageSize = 260.0
+    static let imageMaxHeight = 260.0
+    static let imageTopPadding = 20.0
+    static let imageCornerRadius = 16.0
+    static let imageStrokeWidth = 0.5
+}
+
 struct ForgotMasterPasswordView: View {
 
     @State var presenter: ForgotMasterPasswordPresenter
 
     @Environment(\.dismiss)
     private var dismiss
+    
+    @Environment(\.colorScheme)
+    private var colorScheme
 
     var body: some View {
-        VStack {
+        VStack(spacing: Spacing.xll) {
+            Image(.vaultDecryptionKitFullDocument)
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: Constants.imageMaxHeight)
+                .frame(width: Constants.imageSize)
+                .padding(.top, Constants.imageTopPadding)
+                .background {
+                    RoundedRectangle(cornerRadius: Constants.imageCornerRadius)
+                        .fill(.neutral50)
+                        .stroke(colorScheme == .dark ? .neutral200 : .neutral100, lineWidth: Constants.imageStrokeWidth)
+                }
+            
             HeaderContentView(
                 title: Text(.lockScreenForgotMasterPassword),
-                subtitle: nil
+                subtitle: Text(.lockScreenForgotMasterPasswordDescription)
             )
             .padding(.top, Spacing.l)
             .padding(.bottom, Spacing.m)
@@ -30,6 +53,7 @@ struct ForgotMasterPasswordView: View {
             Spacer(minLength: 0)
         }
         .padding(.horizontal, Spacing.xl)
+        .padding(.bottom, Spacing.m)
         .router(router: ForgotMasterPasswordRouter(), destination: $presenter.destination)
         .toolbar {
             ToolbarItem(placement: .cancellationAction) {
